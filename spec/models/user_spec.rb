226f40fbb3e17fb5976eac99b8f3cb8b3a50838f
janicelaksana <13510035@std.stei.itb.ac.id>
2013-06-04 20:09:33 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,7 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid }
+	it { should respond_to(:remember_token) }
 	it {should respond_to(:authenticate) }
 
 	describe "email address with mixed case" do
@@ -121,4 +122,8 @@ describe User do
 		end
 	end
 
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 end
